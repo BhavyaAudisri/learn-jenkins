97cbd11e67db0e4f2d044c576dabbad372a257ba
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-10 12:30:18 +0530

    code cahnges updated

--- jenkins.tf
+++ jenkins.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "jenkins" {
   ami                    = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  #vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  vpc_security_group_ids = ["sg-02f18e4bed9d09120"]
   instance_type          = "t3.micro"
   tags = merge(
     var.common_tags,
@@ -15,7 +16,8 @@ resource "aws_instance" "jenkins" {
 # jenkins agent creating
 resource "aws_instance" "jenkins-agent" {
   ami                    = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  #vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  vpc_security_group_ids = ["sg-02f18e4bed9d09120"]
   instance_type          = "t3.micro"
   tags = merge(
     var.common_tags,
