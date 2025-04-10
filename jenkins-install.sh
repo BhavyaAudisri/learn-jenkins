c2c99710a50fe2f1babd82c0bf422991eb734998
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-10 12:51:57 +0530

    code cahnges updated

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
